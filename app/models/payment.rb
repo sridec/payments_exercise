8ce8e2f7a867adb04c9f449fe2aded991d2b8c3d
sridec <sri.h0921@gmail.com>
2022-07-01 17:45:16 -0400

    add logger to print the logs to a file

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -9,9 +9,15 @@ class Payment < ActiveRecord::Base
  private
 
  def amount_not_greater_than_outstading_balance_amount
+ 	binding.pry
  	return errors.add(:loan, "must be associated") if !loan
  	return errors.add(:amount, "amount should be grater than 0") if amount <= 0
  	return true if amount <= loan.outstanding_balance
  	errors.add(:amount,"amount can't be greater than outstanding loan balance")
+ 	logger.warn "Payment creation failed due to #{errors.messages} for loan #{loan.id}"
+ end
+
+ def logger
+ 	Logger.new("#{Rails.root}/log/payment_logger_#{Date.today}")
  end
 end
